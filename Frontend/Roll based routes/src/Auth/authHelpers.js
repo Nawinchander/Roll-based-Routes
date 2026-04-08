3e89cf8e1015d67ae1fd66ba6403b0f6f18432f6
Nawin chander <nawinnapzy289@gmail.com>
2026-04-08 18:42:45 +0530

    add

--- Frontend/Roll based routes/src/Auth/authHelpers.js
+++ Frontend/Roll based routes/src/Auth/authHelpers.js
@@ -1,2 +1,9 @@
 /// auth.helpers
 
+import jwtDecode from "jwt-decode";
+
+export const isTokenExpired = (token) => {
+  const decoded = jwtDecode(token);
+  return decoded.exp * 1000 < Date.now();
+};
+
