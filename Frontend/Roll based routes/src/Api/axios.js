1f1b5cfa07558cd357b92d0e3d62f3863e852763
Nawin chander <nawinnapzy289@gmail.com>
2026-04-08 18:41:36 +0530

    add

--- Frontend/Roll based routes/src/Api/axios.js
+++ Frontend/Roll based routes/src/Api/axios.js
@@ -1,1 +1,29 @@
 /// axios
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+});
+
+// REQUEST INTERCEPTOR (token attach)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = token;
+  return config;
+});
+
+// RESPONSE INTERCEPTOR (refresh token)
+api.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    if (err.response.status === 401) {
+      const res = await axios.post("/refresh", {}, { withCredentials: true });
+      localStorage.setItem("token", res.data.accessToken);
+    }
+  }
+);
+
+export default api;
+
