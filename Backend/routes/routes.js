3af2d216ea5f254003359d25c495ede8ceb65143
Nawin chander <nawinnapzy289@gmail.com>
2026-04-08 18:22:02 +0530

    add

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,2 +1,12 @@
 /// routes
 
+const { verifyToken, allowRoles } = require("./middleware/auth");
+
+app.get("/admin", verifyToken, allowRoles("admin"), (req, res) => {
+  res.send("Admin Dashboard");
+});
+
+app.get("/user", verifyToken, allowRoles("user", "admin"), (req, res) => {
+  res.send("User Dashboard");
+});
+
