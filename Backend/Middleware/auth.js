01cca119ac4935ad2b34aed9585d88f6152c8b87
Nawin chander <nawinnapzy289@gmail.com>
2026-04-08 18:21:07 +0530

    add

--- Backend/Middleware/auth.js
+++ Backend/Middleware/auth.js
@@ -1,2 +1,30 @@
 /// auth.js
 
+// middleware/auth.js
+const jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ msg: "No token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // { id, role }
+    next();
+  } catch (err) {
+    return res.status(403).json({ msg: "Invalid token" });
+  }
+};
+
+// Role-based middleware
+exports.allowRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ msg: "Access denied" });
+    }
+    next();
+  };
+};
+
+
