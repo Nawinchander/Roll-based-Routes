989b212454572f9c072361dc7849a011f8d1e1ef
Nawin chander <nawinnapzy289@gmail.com>
2026-04-09 20:02:31 +0530

    add

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,6 +1,22 @@
 /// routes
 
 
+// router.post("/login", login);
+// router.post("/refresh", refreshToken);
+// router.post("/logout", logout);
+
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  login,
+  refresh,
+  logout
+} = require("../controllers/authController");
+
+router.post("/register", register);
 router.post("/login", login);
-router.post("/refresh", refreshToken);
+router.post("/refresh", refresh);
 router.post("/logout", logout);
+
+module.exports = router;
