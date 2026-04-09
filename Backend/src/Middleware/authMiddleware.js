a12366e91e73a3b36e556b72cc4e7463377de5c7
Nawin chander <nawinnapzy289@gmail.com>
2026-04-09 20:00:31 +0530

    add

--- Backend/src/Middleware/authMiddleware.js
+++ Backend/src/Middleware/authMiddleware.js
@@ -1,5 +1,23 @@
-exports.verifyToken = (req, res, next) => {
-  const token = req.cookies.accessToken || req.headers.authorization;
-  // validate JWT here
-};
+// exports.verifyToken = (req, res, next) => {
+//   const token = req.cookies.accessToken || req.headers.authorization;
+//   // validate JWT here
+// };
 
+
+
+const jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+  const token =
+    req.cookies.accessToken || req.headers.authorization;
+
+  if (!token) return res.sendStatus(401);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.sendStatus(403);
+  }
+};
