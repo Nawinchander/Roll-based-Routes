6752b3eaec8860550dc4c6749bc666d250d8db10
Nawin chander <nawinnapzy289@gmail.com>
2026-04-09 20:03:00 +0530

    add

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,6 +1,23 @@
-router.get(
-  "/admin",
-  verifyToken,
-  allowRoles("admin"),
-  getAdminData
-);
+// router.get(
+//   "/admin",
+//   verifyToken,
+//   allowRoles("admin"),
+//   getAdminData
+// );
+
+
+
+const express = require("express");
+const router = express.Router();
+const { verifyToken } = require("../middleware/authMiddleware");
+const { allowRoles } = require("../middleware/roleMiddleware");
+
+router.get("/admin", verifyToken, allowRoles("admin"), (req, res) => {
+  res.json({ msg: "Admin data" });
+});
+
+router.get("/user", verifyToken, (req, res) => {
+  res.json({ msg: "User data" });
+});
+
+module.exports = router;
