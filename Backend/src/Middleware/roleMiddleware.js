b8952f7200dde57fd53f6fd393844acb4ffe41e2
Nawin chander <nawinnapzy289@gmail.com>
2026-04-09 20:01:07 +0530

    add

--- Backend/src/Middleware/roleMiddleware.js
+++ Backend/src/Middleware/roleMiddleware.js
@@ -1,7 +1,20 @@
+// exports.allowRoles = (...roles) => {
+//   return (req, res, next) => {
+//     if (!roles.includes(req.user.role)) return res.sendStatus(403);
+//     next();
+//   };
+// };
+
+
+
 exports.allowRoles = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) return res.sendStatus(403);
+    if (!roles.includes(req.user.role)) {
+      return res.sendStatus(403);
+    }
     next();
   };
 };
+
+
 
