0b9b8bafee08077b1952e9d929825793807a286c
Nawin chander <nawinnapzy289@gmail.com>
2026-04-09 20:03:25 +0530

    add

---
+++ Backend/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+module.exports = app;
+
